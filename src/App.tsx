223177abda96b535eef5026abfb70380a7f10855
gabriel-santana <gabrielsantana77@hotmail.com>
2022-10-15 22:06:54 -0300

    Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Button } from './components/Button'
 import './styles/global.css'
 
 export function App() {
@@ -5,9 +6,8 @@ export function App() {
     <>
       <h1 className="font-bold text-2xl text-violet-500">Hello</h1>
     
-      <button className="bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-cyan-300 ">
-      Enviar
-      </button>
+      <Button children="Um botão"/>
+
     </>
   )
 }
